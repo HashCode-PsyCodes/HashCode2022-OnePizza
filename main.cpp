e51bd8135307fb19dff87ff43ea7846767722589
Dinushi Ariyasena <87066711+DinuAR@users.noreply.github.com>
2022-02-15 09:30:58 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+
+  int numOfCus;
+
+  cin>>numOfCus;
+
+  set<string> likes;
+
+  for(int i=0; i<numOfCus; i++){
+
+    int numOfLikes;
+    cin>> numOfLikes;
+
+    for(int x=0; x<numOfLikes; x++) {
+      string lk;
+      cin >> lk;
+      likes.insert(lk);
+    }
+
+    int numOfDLikes;
+    cin>> numOfDLikes;
+
+    for(int x=0; x<numOfDLikes; x++) {
+      string dlk;
+      cin >> dlk;
+      likes.erase(dlk);
+    }
+
+  }
+
+  cout << likes.size() << " ";
+
+  for (auto it: likes) {
+      cout << it << " ";
+  }
+
+  return 0;
+}
